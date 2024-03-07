adcca62e84c832d47cc9cd3d1553663436424a7e
Dmytro Protsenko <dprotsenko.repos@gmail.com>
2024-03-07 15:27:44 +0200

    add mongo

---
+++ mongo/mongo-init.js
@@ -0,0 +1,23 @@
+db = db.getSiblingDB('sales')
+
+db.createUser({
+    user: 'sales',
+    pwd: 'sales',
+    roles: [{ role: 'readWrite', db: 'sales' }],
+});
+
+db.createCollection('receipts')
+db.createCollection('documents')
+db.createCollection('invoices')
+
+db = db.getSiblingDB('warehouse')
+
+db.createUser({
+    user: 'warehouse',
+    pwd: 'warehouse',
+    roles: [{ role: 'readWrite', db: 'warehouse' }],
+});
+db.createCollection('documents')
+db.createCollection('stocks')
+db.createCollection('invoices')
+db.createCollection('orders')
---
+++ mongo/readme.md
@@ -0,0 +1,25 @@
+# MongoDB
+
+### Connections
+
+#### Sales
+
+```
+mongodb://sales:sales@localhost:27017/sales
+```
+
+#### Warehouse
+
+```
+mongodb://warehouse:warehouse@localhost:27017/warehouse
+```
+
+### References
+
+https://hub.docker.com/_/mongo
+
+https://hub.docker.com/_/mongo-express
+
+https://dev.to/mikelogaciuk/docker-initialize-custom-users-and-databases-in-mongodb-3dkb
+
+https://stackoverflow.com/questions/42912755/how-to-create-a-db-for-mongodb-container-on-start-up
